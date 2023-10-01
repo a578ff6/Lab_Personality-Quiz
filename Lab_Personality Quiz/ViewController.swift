93904a943701723b63740888d206c9b7882ca6a6
wayne tsao <a578ff6@gmail.com>
2023-10-02 03:39:27 +0800

    Initial Commit

---
+++ Lab_Personality Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_Personality Quiz
+//
+//  Created by 曹家瑋 on 2023/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
